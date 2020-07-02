3d6a4d6f4dd7e0fa1d021c6c9deb72849a063c0c
lucas861223 <lucas861223@gmailcom>
2020-07-02 16:06:15 -0700

    upadte to v5

--- api/index.py
+++ api/index.py
@@ -6,10 +6,11 @@ class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         username = parse_qs(urlparse(self.path).query)['username'][0]
-        response = requests.get(url="https://api.twitch.tv/helix/users?login=" + username,
-                                headers={"client-id": "w07tun6ja438fsymn61ei88tm8kw7q"})
+        response = requests.get(url="https://api.twitch.tv/kraken/users?login=" + "lucas861223",
+                                headers={"client-id": "xnpbm69um56zzge4yh6ojhg1rg23s1",
+                                         "Accept": "application/vnd.twitchtv.v5+json"})
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
-        self.wfile.write(response.json()['data'][0]['id'].encode())
+        self.wfile.write(response.json()['users'][0]['_id'].encode())
         return
